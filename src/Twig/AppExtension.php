f591893352bd45c8261c25cd80bbfbe406ecee4b
cuma561 <krawczakbartek@gmail.com>
2022-07-31 14:48:04 +0200

    Change date to year -> tag name to function getYear

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -41,7 +41,7 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFunction('locales', [$this, 'getLocales']),
-            new TwigFunction('date', [$this, 'getYear']),
+            new TwigFunction('year', [$this, 'getYear']),
         ];
     }
 
