d17e5cd7791f3b1a17b15b6d3f1b82c49f13855c
cuma561 <krawczakbartek@gmail.com>
2022-08-31 13:05:09 +0200

    User List
    - pobieranie danych wszystkich uzytkownikow
    - zmiana routingu/przekierowania do podstrony
    - zrobienie odpowiedniej tabeli oraz przyciskow
    - dodanie nowych translakcji

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -3,11 +3,30 @@
 namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 
 class UserController extends AbstractController
 {
-	public function index() {
+	public function users(UserRepository $users) {
 
-		return $this->render('admin/user/user.html.twig');
+		$userList = $users->findAll($users);
+
+		return $this->render('admin/user/user.html.twig', ['users' => $userList]);
+	}
+
+	public function show() {
+
+	}
+
+	public function new() {
+
+	}
+
+	public function edit(){
+
+	}
+
+	public function delete() {
+
 	}
 }
