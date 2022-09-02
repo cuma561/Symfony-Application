25aea1c194f8a972263b74968ca328f5c772a959
cuma561 <krawczakbartek@gmail.com>
2022-09-02 10:02:32 +0200

    ShowTags
    - add new routing
    - add new view -> show tag
    - add new method controller -> fetch and render show tag
    - add link to button show tag
    - add new translations

--- src/Controller/Admin/TagController.php
+++ src/Controller/Admin/TagController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Tag;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,5 +15,10 @@ class TagController extends AbstractController
 		$tagsList = $tags->findAll($tags);
 
 		return $this->render('admin/tag/tag.html.twig', ['tags' => $tagsList]);
+	}
+
+	public function show(Tag $tag): Response {
+
+		return $this->render('admin/tag/show.html.twig', ['tag' => $tag]);
 	}
 }
