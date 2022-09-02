1f3df74ae454dffecfc9b13be4e280c59532c46d
cuma561 <krawczakbartek@gmail.com>
2022-09-02 10:29:50 +0200

    DeleteTag
    - add new routing
    - add new button -> delete tag
    - add new modal -> delete tag
    - add new method -> delete tag
    - add new translations

--- src/Controller/Admin/TagController.php
+++ src/Controller/Admin/TagController.php
@@ -6,6 +6,7 @@ use App\Entity\Tag;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class TagController extends AbstractController
 {
@@ -20,5 +21,20 @@ class TagController extends AbstractController
 	public function show(Tag $tag): Response {
 
 		return $this->render('admin/tag/show.html.twig', ['tag' => $tag]);
+	}
+
+	public function delete(Request $request, Tag $tag): Response {
+		
+		if (!$this->isCsrfTokenValid('delete', $request->request->get('token'))) {
+            return $this->redirectToRoute('admin_tags_index');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($tag);
+        $em->flush();
+
+        $this->addFlash('success', 'tag.deleted_successfully');
+
+        return $this->redirectToRoute('admin_tags_index');
 	}
 }
